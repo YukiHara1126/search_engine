38838cc3d8b72ebc8b3c54d39ad8cbb86843f8d1
Yuki Hara <yuki.hara1126@gmail.com>
2022-03-27 16:28:32 +0900

    add get url fn

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
-fn main() -> eyre::Result<()> {
-    let body = reqwest::blocking::get("https://www.rust-lang.org")?
-        .text()?;
+use select::document::Document;
+use select::predicate::Name;
+use url::Url;
+use url::ParseError as UrlParseError;
+fn main() -> eyre::Result<()>{
+    let response = reqwest::blocking::get("https://www.e-casebook.com/")?;
+    let base_url = response.url().clone();
+    let body = response.text()?;
 
-    println!("body = {:?}", body);
+    let doc = Document::from(body.as_str());
+    println!("body = {:?}", doc);
+    for href in doc.find(Name("a")).filter_map(|a| a.attr("href")){
+        println!("{:?}",Url::parse(href));
+        match Url::parse(href){
+            Ok(url) => {println!("{}",url);},
+            Err(UrlParseError::RelativeUrlWithoutBase) => {
+                let url = base_url.join(href)?;
+                println!("{}",url);
+            }
+            Err(e) => {},
+        }
+    }
 
     Ok(())
 }
